803906125f5ce2d312011d1b8cc514479a54fa22
kenny sanders <kenny@appliedfoo.com>
2009-12-03 21:19:02 -0500

    * ast: get_node method returns the appropriate Node type * parser: tree building methods

--- somersault/parser.py
+++ somersault/parser.py
@@ -11,21 +11,78 @@ class Parser(object):
     def __init__(self, source):
         self.lexer = Lexer(source)
         self.tokens = []
+        self.nodes = [] # stack holding the AST nodes
 
     def parse(self):
         """
         Parse the input stream into an AST.
         """
-        self.tokens = list(self.lexer)
+        self.next = self.lexer.next()
 
     def print_tokens(self):
         for token in self.tokens:
             print token
 
+    def build_child(self, token):
+        self.nodes.append(get_node(token))
+
+    def build_parent(self, token, num_children=0):
+        parent = get_node(token)
+
+        children = [] # stack of children Nodes
+        for child in range(num_children):
+            children.append(self.pop_node())
+
+        for child in range(num_children, 0, -1):
+            children[child].sibling = children[child - 1]
+
+        if num_children > 0:
+            parent.child = children[num_children - 1]
+
+        self.nodes.append(parent)
+
+    def pop_node(self):
+        """
+        Pop the tree node from the top of the stack.
+        """
+        return self.nodes.pop()
+            
+    def read(self, token):
+        """
+        Read an expected value. If token != next, error.
+        If token is a rand, build a childless tree.
+        """
+        if token.type in tokens.rand:
+            pass # build_tree
+        
+        if self.next is not token:
+            error("Expected %s but found %s on line %d" % (next.value, token.value, next.lineno))
+
+        next = self.lexer.next()
+
+    #### Grammar specific methods ####
+    def expression(self):
+        if next is "let":
+            read("let")
+            # D()
+            read("in")
+            # expression()
+            # build_tree(let)
+        elif next is "fn":
+            read("fn")
+            # for each parameter:
+            #    variable()
+            # read(".")
+            # expression()
+            # build_tree(lambda)
+        else:
+            pass # where()
+    ##################################
+
 def main():
-    p = Parser(file(sys.argv[1]).read())
-    p.parse()
-    p.print_tokens()
+    pass #p = Parser(file(sys.argv[1]).read())
+    #p.parse()
+    #p.print_tokens()
     
 if __name__ == "__main__":
     main()
